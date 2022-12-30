b9bdc89ecb8abaaacc08c49e512882a2c6cbce81
zccst <zccst@qq.com>
2022-12-30 17:16:19 +0800

    add global directory

---
+++ okc-docs.js
@@ -0,0 +1,216 @@
+[
+    {
+        "title": "About OKC",
+        "children": [
+            {
+                "title": "How To Use",
+                collapsable: false,
+                "children": ['about-okc/quick-start']
+            }
+        ]
+    },
+    {
+        "title": "Official Dapps",
+        "children": [
+            {
+                "title": "Liquid Staking",
+                collapsable: false,
+                "children": ['official-dapps/liquid-staking']
+            }
+        ]
+    },
+    {
+        "title": "Developer Tools",
+        "children": [
+            {
+                "title": "Gas Back",
+                collapsable: false,
+                "children": [
+                    "developer-tools/gas-back/developer-dividend-project",
+                    "developer-tools/gas-back/developer-dividend-project-operation-guide"
+                ]
+            },
+            {
+                "title": "Oracle",
+                collapsable: false,
+                "children": [
+                    "developer-tools/oracle/oracle"
+                ]
+            },
+            {
+                "title": "EVM",
+                collapsable: false,
+                "children": [
+                    "developer-tools/evm/evm-integration-guide"
+                ]
+            }
+        ]
+    },
+    {
+        "title": "Dev Docs",
+        "children": [
+            {
+                "title": "OKC",
+                "children": [
+                    "dev/okc/overview"
+                ]
+            },
+            {
+                "title": "Getting Start",
+                "children": [
+                    "dev/getting-start/install-okc",
+                    "dev/getting-start/deploy-you-own-okc-testnet",
+                    "dev/getting-start/join-okc-testnet",
+                    "dev/getting-start/join-okc-testnet-with-docker",
+                    "dev/getting-start/join-okc-mainnet",
+                    "dev/getting-start/upgrade-node",
+                    "dev/getting-start/install-faq"
+                ]
+            },
+            {
+                "title": "Developers",
+                "children": [
+                    "dev/developers/quick-start-for-mainnet",
+                    "dev/developers/quick-start",
+                    {
+                        "title": "Basics",
+                        "children": [
+                            "dev/developers/basics/accounts",
+                            "dev/developers/basics/gas",
+                            "dev/developers/basics/transactions",
+                            "dev/developers/basics/websocket"
+                        ]
+                    },
+                    {
+                        "title": "Tools",
+                        "children": [
+                            "dev/developers/tools/create-wallet",
+                            "dev/developers/tools/hardhat",
+                            "dev/developers/tools/remix",
+                            "dev/developers/tools/smart-contract",
+                            "dev/developers/tools/truffle",
+                            "dev/developers/tools/web3js"
+                        ]
+                    },
+                    {
+                        "title": "Deploy",
+                        "children": [
+                            "dev/developers/deploy/remix",
+                            "dev/developers/deploy/truffle",
+                            "dev/developers/deploy/verify-contract",
+                            "dev/developers/deploy/verify-proxy-contract"
+                        ]
+                    },
+                    {
+                        "title": "KIP20 Tokens",
+                        "children": [
+                            "dev/developers/KIP20Tokens/introduction",
+                            "dev/developers/KIP20Tokens/issue-token",
+                            "dev/developers/KIP20Tokens/kip20"
+                        ]
+                    },
+                    {
+                        "title": "Blockchain Detail",
+                        "children": [
+                            "dev/developers/blockchainDetail/aminorpc",
+                            "dev/developers/blockchainDetail/okexchain-fullnode",
+                            "dev/developers/blockchainDetail/endpoint"
+                        ]
+                    },
+                    "dev/developers/FAQ"
+                ]
+            },
+            {
+                "title": "Delegators",
+                "children": [
+                    "dev/delegators/delegators-overview",
+                    "dev/delegators/delegators-guide-cli",
+                    "dev/delegators/delegators-staking-cli",
+                    "dev/delegators/sending-stakingtx-using-evmtx",
+                    "dev/delegators/delegators-faq"
+                ]
+            },
+            {
+                "title": "Validators",
+                "children": [
+                    "dev/validators/validators-overview",
+                    "dev/validators/validators-guide-cli",
+                    {
+                        "title": "Detail",
+                        "children": [
+                            "dev/validators/detail/distr"
+                        ]
+                    },
+                    "dev/validators/validators-faq"
+                ]
+            },
+            {
+                "title": "Gas Back",
+                "children": [
+                    "dev/gas-back/how-to-get-gas-back-on-okc"
+                ]
+            },
+            {
+                "title": "IBC",
+                "children": [
+                    "dev/ibc/ibc",
+                    "dev/ibc/ibc-cli"
+                ]
+            },
+            {
+                "title": "Wasm",
+                "children": [
+                    "dev/wasm/overview",
+                    "dev/wasm/instruction-manual",
+                    "dev/wasm/vmbridge",
+                    "dev/wasm/cosmwasmjs"
+                ]
+            },
+            {
+                "title": "Concepts",
+                "children": [
+                    "dev/concepts/general-concepts",
+                    "dev/concepts/gov",
+                    "dev/concepts/sentry-nodes",
+                    "dev/concepts/two-format-address"
+                ]
+            },
+            {
+                "title": "Resources",
+                "children": [
+                    "dev/resources/genesis",
+                    "dev/resources/exchaincli",
+                    "dev/resources/service-providers",
+                    "dev/resources/snapshot"
+                ]
+            },
+            {
+                "title": "Tools",
+                "children": [
+                    "dev/tools/monitor"
+                ]
+            },
+            {
+                "title": "API",
+                "children": [
+                    {
+                        "title": "SDK",
+                        "children": [
+                            "dev/api/sdk/go-sdk",
+                            "dev/api/sdk/java-sdk",
+                            "dev/api/sdk/js-sdk"
+                        ]
+                    },
+                    "dev/api/http",
+                    "dev/api/json_rpc"
+                ]
+            },
+            {
+                "title": "Link",
+                "children": [
+                    "dev/references/link"
+                ]
+            }
+        ]
+    }
+]
