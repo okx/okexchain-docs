9f8cac1f8849e20ccff2b39292b8df2dbb16449b
kasrakhosravi <kasrakhosravi2@gmail.com>
2023-09-22 16:34:30 +0200

    feat: add flair indexing

--- oktc-docs.js
+++ oktc-docs.js
@@ -289,6 +289,13 @@ module.exports = [
           "dev/developer-services/gas-back/how-to-get-gas-back-on-oktc",
         ],
       },
+      {
+        title: "Data Indexing",
+        collapsable: false,
+        children: [
+          "dev/developer-services/data-indexing/indexing-okc-onchain-data",
+        ],
+      },
     ],
   },
   {
